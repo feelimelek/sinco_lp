6bc5364f378d961968c6533195b23df19733fea0
feelimelek <80131744+feelimelek@users.noreply.github.com>
2021-12-16 23:03:43 -0300

    Add files via upload

---
+++ 35-anos.php
@@ -0,0 +1,203 @@
+<?php get_header(); ?>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<div class="breadcrumbs">
+		<div class="container">
+			<div class="pure-g">
+				<div class="pure-u-1">
+				<?php if ( function_exists('yoast_breadcrumb') ) 
+				{yoast_breadcrumb('<p id="breadcrumbs">','</p>');} ?>
+				</div>
+			</div>
+		</div>
+	</div>
+	<hr class="hr-1">
+	<div class="container-fluid div-principal bg-img">
+		<section>
+			<div class="container-fluid div-img-sobrepostas">
+        		<img src="assets/images/01_sinco_landingpage_layout_foto_35_anos.png" alt="sinco-35-anos" class="img-fluid" id="imagem-35-anos" >
+        		<div class="div-img-vermelha container">
+        		<img src="assets/images/01_sinco_landingpage_layout_retangulovermelho.png" alt="fundo-vermelho" class="img-fluid" id="imagem-vermelha">
+            	<h1>Há 35 anos estamos<br>
+                	aprimorando nossa<br>
+                	maneira de interpretar<br>
+                	e realizar os sonhos<br>
+                	dos nossos clientes.</h1>
+        		</div>
+        		<img src="assets/images/01_sinco_landingpage_layout_bolinhas.png" alt="quadrado-de-bolinhas" class="img-fluid" id="imagem-pontinhos-1">
+    		</div>
+		</section>
+		<section>
+			<div class="container div-texto-paragrafo-1">
+		        <p>Durante nossa trajetória, construímos e incorporamos orgulhosamente mais de <br>
+		            <b class="bold">3,5 milhões de metros quadrados.</b> Nascemos como <b class="b-red">S</b><b>ociedade </b> <b class="b-red">In</b><b>corporadora<br>
+		            e</b> <b class="b-red"> Co</b><b>nstrutora </b> – origem do nosso nome atual, Sinco – e nosso portfólio <br>
+		            compreende majoritariamente construções de médio e alto padrão em diversos<br>
+		            segmentos: residencial, corporativo, hoteleiro, shopping, teatro, escola, indústria,<br>
+		            telecomunicação, entre outros. <b>São mais de 160 empreendimentos localizados<br>
+		            entre São Paulo e Rio de Janeiro.</b>
+		        </p>
+		    </div>
+		</section>
+		<section>
+			<div class="container-fluid div-texto-2-e-img">
+		        <div class="container div-texto-paragrafo-2">
+		            <p>O início dessa história foi em <b>1986,</b><br>
+		            quando três irmãos e um amigo se<br>
+		            juntaram para empreender no ramo<br>
+		            da construção. José Luiz, Paulo,<br>
+		            Tadeu e Tony, sócios fundadores,<br>
+		            continuam até hoje trabalhando a<br>
+		            todo vapor para a empresa crescer<br>
+		            cada vez mais consistente com<br>
+		            seus valores e entregar soluções<br>
+		            inovadoras. São mais de <b>três<br>
+		            décadas de credibilidade e tradição</b><br>
+		            no mercado, construídos com muito<br>
+		            esforço e dedicação, pensando<br>
+		            desde o primeiro dia em entregar<br>
+		            qualidade e confiança.<br
+		            ></p>
+		        </div>
+		        <div class="container div-img-predio-1">
+		            <img src="../../assets/images/01_sinco_landingpage_layout_foto 1986.png" alt="predio-1986" id="img-predio-1">
+		        </div>
+		    </div>
+		</section>
+		<section>
+		    <div class="container div-texto-paragrafo-3">
+		        <p>
+		            De obra em obra, fomos ampliando nosso time e atualmente contamos com<br>
+		            mais de mil profissionais altamente qualificados, que entraram para a família<br>
+		            garantindo <b>eficiência e diferencial em todos os nossos projetos.</b><br>
+		        </p>
+		    </div>
+		</section>
+		<section>
+		    <div class="container-fluid div-titulo1-e-linha-vermelha">
+		        <span class="span-linha-vermelha-vertical-1">
+		            <img src="../../assets/images/01_sinco_landingpage_layout_linha vermelha vertical.png" alt="linha-vermelha-vertical" class="img-fluid" id="linha-vermelha-vertical-1">
+		        </span>
+		        <h2 id="h1-2">O que faz a <font style="color: #d93a2f">Sinco Engenharia</font> se destacar no<br>
+		            mercado e ser reconhecida como uma referência?</h2>
+		    </div>
+		</section>
+		<section>
+		    <div class="container div-texto-paragrafo-4">
+		        <p>
+		            Buscamos incansavelmente melhorias em nossas obras e temos orgulho de<br>
+		            estarmos <b>sempre à frente das inovações na construção civil,</b> sendo uma<br>
+		            das precursoras no Brasil a utilizar a <b>plataforma BIM</b> (Building Information<br>
+		            Modeling) – com vantagens como aumento de produtividade, redução de<br>
+		            perdas, abreviação de prazos, assertividade dos orçamentos e o fundamental,<br>
+		            qualidade do produto imobiliário.
+		        </p>
+		    </div>
+		</section>
+		<section>
+		    <div class="container div-texto-5-e-img">
+		        <div class="div-img-predio-2 container">
+		            <img src="../../assets/images/01_sinco_landingpage_layout_foto predio horizontal.png" alt="predio-horizontal" class="img-fluid" id="img-predio-2">
+		            <div class="container div-img-bolinhas-2">
+		                <img src="../../assets/images/01_sinco_landingpage_layout_bolinhas.png" alt="" class="img-fluid" id="img-pontinhos-2">
+		            </div>
+		        </div>
+		        <div class="container div-texto-paragrafo-5">
+		            <p>
+		                <b>Temos a <font style="color: #d93a2f">“Engenharia<br>
+		                de Resultados”</font>, desde<br>
+		                o início, como nossa<br>
+		                principal virtude, focados<br>
+		                em oferecer o melhor para<br>
+		                os nossos clientes,<br>
+		                atendendo as necessidades<br>
+		                específicas de cada um<br>
+		                deles e entregando<br>
+		                empreendimentos únicos.</b>
+		            </p>
+		            <div class="container span-linha-vermelha-horizontal-1">
+		                <img src="../../assets/images/01_sinco_landingpage_layout_linha vermelha horizontal.png" alt="linha-vermelha-horizontal" class="img-fluid" id="linha-vermelha-horizontal-1">
+		            </div>
+		        </div>
+		    </div>
+		</section>
+		<section>
+		    <div class="container div-texto-paragrafo-6">
+		        <p>
+		            A Sinco entende que entregar um projeto com <b>alta qualidade<br>
+		            e tecnologia</b> que perdure, não é apenas um diferencial, é um<br>
+		            compromisso que norteia cada escolha, que envolve o criar e <br>
+		            o executar do melhor produto, para todos os que desejam ter<br>
+		            uma vivência em um <b>empreendimento Sinco.</b>
+		        </p>
+		    </div>
+	    </section>
+	    <section>
+		    <div class="container-fluid div-texto-6-e-img">
+		        <div class="container div-texto-paragrafo-7-8-9">
+		            <p>
+		                Edificar com excelência e buscar<br>
+		                sempre a melhoria contínua de<br>
+		                nossos processos, visando atender<br>
+		                às singularidades de cada um dos<br>
+		                nossos clientes.
+		            </p>
+		            <div class="container-fluid div-titulo2-e-linha-vermelha">
+		                <span class="span-linha-vermelha-vertical-1">
+		                    <img src="../../assets/images/01_sinco_landingpage_layout_linha vermelha vertical.png" alt="linha-vermelha-vertical" class="img-fluid" id="linha-vermelha-vertical-2">
+		                </span>
+		                <h2 id="h1-2">Essa é a<br> nossa missão.</h2>
+		            </div>
+		            <p>
+		                Temos orgulho da nossa história e<br>
+		                estamos ansiosos para saber quais<br>
+		                conquistas nos aguardam. 
+		            </p>
+		            <br>
+		            <p>
+		                Acesse o nosso <b>Instagram</b> para saber<br>
+		                um pouco mais sobre nossa trajetória.
+		            </p>
+		        </div>
+		        <div class="container div-img-predio-3">
+		            <img src="../../assets/images/01_sinco_landingpage_layout_foto predio vertical.png" alt="predio-vertical" id="img-predio-3">
+		            <div class="container div-img-bolinhas-3">
+		                <img src="../../assets/images/01_sinco_landingpage_layout_bolinhas.png" alt="bolinhas" class="img-fluid" id="img-pontinhos-3">
+		            </div>
+		            <div class="container div-img-bolinhas-4">
+		                <img src="../../assets/images/01_sinco_landingpage_layout_bolinhas.png" alt="bolinhas" class="img-fluid" id="img-pontinhos-4">
+		            </div>
+		        </div>
+		    </div>
+		</section>
+		<section id="topo-sobre">
+			<div class="container">
+				<div class="pure-g bgbranco">
+					<div class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-3">
+					<h1>CONHEÇA A<br><span>SINCO</span></h1>
+				</div>
+				<div class="pure-u-0 pure-u-md-1-2 pure-u-lg-1-3 img-topo">
+					<img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/conheca-a-sinco.jpg" class="pure-img">
+				</div>
+				<div class="pure-u-0 pure-u-md-1-2 pure-u-lg-1-3">
+					<div class="quote">
+						<h2>CREDIBILIDADE E<br>
+						<strong>TRADIÇÃO NO MERCADO</strong></h2>
+						<p>Mais de 3 milhões de m² em nosso portfolio.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+<div style="display:none;">
+	<div id="bimBox" class="pure-u-g">
+		<div class="pure-u-1">
+		<?php the_field('mais_informacoes_bim'); ?>
+		</div>
+	</div>
+</div>
+
+	<?php endwhile; else : ?>
+	<p><?php _e( 'Desculpe, nenhum resultado encontrado.' ); ?></p>
+	<?php endif; ?>
+<?php get_footer(); ?>
